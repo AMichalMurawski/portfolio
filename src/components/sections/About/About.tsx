bc24a4df73c278de5d82ea66df0d1454cbca1b6f
Michal M <m.a.murawski@wp.pl>
2023-09-28 23:33:17 +0200

    Update About.tsx

--- src/components/sections/About/About.tsx
+++ src/components/sections/About/About.tsx
@@ -17,6 +17,31 @@ export const About: React.FC = () => {
         at fugit debitis hic suscipit, aspernatur quod nesciunt exercitationem
         cum natus libero sed quae itaque, expedita numquam tenetur minus.
       </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, alias
+        assumenda! Delectus, dignissimos minima ullam impedit inventore
+        assumenda molestias consectetur! Excepturi laborum consequatur nesciunt
+        obcaecati aperiam eaque quisquam aliquam, doloremque earum nisi
+        inventore distinctio harum necessitatibus veniam? Officiis molestias
+        suscipit numquam consequatur perspiciatis? Quidem ratione placeat facere
+        eum porro corporis itaque quas velit, et neque error? Quod nobis sed
+        repellendus.
+      </Paragraph>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident nobis
+        numquam, minus quam blanditiis sequi sint iusto quas exercitationem
+        mollitia quia. Veniam placeat inventore porro corrupti rerum esse nam
+        adipisci, architecto ad repellendus dolor totam repellat nulla
+        necessitatibus rem maxime in similique consequatur odio vitae sunt
+        veritatis, a quisquam? Facilis maiores ipsa minima mollitia ipsum vitae
+        neque ea quo expedita eius iusto, perspiciatis quisquam atque porro sed
+        earum a rem nam nulla. Adipisci ipsum illo necessitatibus autem ducimus
+        eaque quaerat nam provident iusto. Commodi itaque ea placeat odio
+        asperiores magni sapiente quae nobis, doloremque perspiciatis!
+        Dignissimos soluta totam mollitia numquam perspiciatis quidem sed nulla
+        reiciendis fugiat, accusantium eveniet error, modi alias! Soluta natus
+        laborum similique unde. Beatae, natus repudiandae! Et?
+      </Paragraph>
     </AboutWrapper>
   );
 };
