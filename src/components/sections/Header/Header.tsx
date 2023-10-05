daad3088ab202e86112a76167d819fb6b52c430d
Michal M <m.a.murawski@wp.pl>
2023-10-05 13:53:25 +0200

    create dark mode background sketch

--- src/components/sections/Header/Header.tsx
+++ src/components/sections/Header/Header.tsx
@@ -1,39 +1,52 @@
 import React from 'react';
-import { ButtonsList, HeaderBox, HeaderWrapper } from './Header.styled';
+import {
+  ButtonsList,
+  HeaderBox,
+  HeaderConst,
+  HeaderWrapper,
+} from './Header.styled';
 import { ButtonLink } from '../../elements/buttons';
 import { links } from '../../../context';
+import { useMode } from '../../../providers';
+import { DarkModeAnimation } from '../../elements/ui';
 
 export const Header: React.FC = () => {
+  const { toggleMode } = useMode();
+
   return (
-    <HeaderBox>
-      <HeaderWrapper>
-        <div
-          style={{
-            width: '80px',
-            height: '80px',
-            borderRadius: '50%',
-            color: 'white',
-            backgroundColor: 'red',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 0,
-            lineHeight: 1,
-            boxShadow: 'inset 0 0 10px 4px gray',
-          }}
-        >
-          Logo
-        </div>
-        <ButtonsList>
-          {links.map(link => (
-            <ButtonLink
-              key={link.path}
-              caption={link.name}
-              linkTo={link.path}
-            />
-          ))}
-        </ButtonsList>
-      </HeaderWrapper>
-    </HeaderBox>
+    <>
+      <HeaderConst onClick={toggleMode}></HeaderConst>
+      <HeaderBox onClick={toggleMode}>
+        <DarkModeAnimation />
+        <HeaderWrapper>
+          <div
+            style={{
+              width: '80px',
+              height: '80px',
+              borderRadius: '50%',
+              color: 'white',
+              backgroundColor: 'red',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: 0,
+              lineHeight: 1,
+              boxShadow: 'inset 0 0 10px 4px gray',
+            }}
+          >
+            Logo
+          </div>
+          <ButtonsList>
+            {links.map(link => (
+              <ButtonLink
+                key={link.path}
+                caption={link.name}
+                linkTo={link.path}
+              />
+            ))}
+          </ButtonsList>
+        </HeaderWrapper>
+      </HeaderBox>
+    </>
   );
 };
