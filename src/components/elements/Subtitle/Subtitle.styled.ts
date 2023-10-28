491f677ba24ee8cac37ba348f3ecd23e2cba62c9
Michal M <m.a.murawski@wp.pl>
2023-10-28 17:56:47 +0200

    font + color

--- src/components/elements/Subtitle/Subtitle.styled.ts
+++ src/components/elements/Subtitle/Subtitle.styled.ts
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
 import { H2Element } from '../../../utils/styles';
 
-export const Sub = styled(H2Element)``;
+export const Sub = styled(H2Element)`
+  font-family: 'Nosifer';
+  color: ${props => props.theme.color.subtilte};
+  font-weight: 400;
+`;
