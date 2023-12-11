e38861a70ffb1b5e9a6ff8467c3cf726059267ec
Michał Murawski <m.a.murawski@wp.pl>
2023-12-11 15:27:22 +0100

    billboard frame design

--- src/components/sections/Projects/Projects.tsx
+++ src/components/sections/Projects/Projects.tsx
@@ -10,35 +10,38 @@ import { Project } from '../../ui';
 import { Autoplay, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import { ScrollBoard } from './ScrollBoard/ScrollBoard';
 
 export const Projects: React.FC = () => {
   return (
     <ProjectWrapper id="projects">
       <Subtitle>My Projects</Subtitle>
-      <ProjectsContener
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        slidesPerView={'auto'}
-        centeredSlides={true}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
-        scrollbar={{}}
-        modules={[Autoplay, Navigation]}
-      >
-        {projects.map((project, i) => (
-          <ProjectItem>
-            <Project
-              description={project.name}
-              tools={project.tools}
-              image={project.images[0]}
-            />
-          </ProjectItem>
-        ))}
-      </ProjectsContener>
+      <ScrollBoard>
+        <ProjectsContener
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          slidesPerView={'auto'}
+          centeredSlides={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          scrollbar={{}}
+          modules={[Autoplay, Navigation]}
+        >
+          {projects.map((project, i) => (
+            <ProjectItem key={i}>
+              <Project
+                description={project.name}
+                tools={project.tools}
+                image={project.images[0]}
+              />
+            </ProjectItem>
+          ))}
+        </ProjectsContener>
+      </ScrollBoard>
     </ProjectWrapper>
   );
 };
