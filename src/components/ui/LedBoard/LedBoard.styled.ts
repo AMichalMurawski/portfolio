251cc078bb985632a28de51d1fd1bfc7ca470cbe
Michal M <m.a.murawski@wp.pl>
2024-01-17 23:10:56 +0100

    led colors + hover HashLink

--- src/components/ui/LedBoard/LedBoard.styled.ts
+++ src/components/ui/LedBoard/LedBoard.styled.ts
@@ -9,10 +9,10 @@ export const LedBoardFrame = styled.div`
   max-width: 50%;
   z-index: 1;
   min-width: min-content;
-  background-color: gray;
+  background-color: rgb(0, 0, 0);
   background-image: radial-gradient(
     circle at ${dimension / 2}px ${dimension / 2}px,
-    lightgray ${(0.55 * dimension) / 2}px,
+    rgb(100, 100, 100) ${(0.55 * dimension) / 2}px,
     transparent 0
   );
   background-size: ${dimension}px ${dimension}px;
@@ -25,12 +25,6 @@ export const LedConteiner = styled.nav`
   row-gap: ${2 * dimension}px;
   column-gap: ${5 * dimension}px;
   // justify-content: center;
-`;
-
-export const Phrase = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: ${dimension}px;
 `;
 
 export const HashLink = styled(Link)`
@@ -65,8 +59,18 @@ export const LightPoint = styled.div`
   transform: translate(-50%, -50%);
   width: 85%;
   height: 85%;
-  background-color: rgb(225, 255, 200);
-  box-shadow: 0 0 16px 1px rgba(160, 255, 255, 0.6),
-    0 0 32px 1px rgba(144, 255, 255, 0.6);
+  background-color: #b9d9eb;
+  box-shadow: 0 0 32px 1px #b2ffff, 0 0 64px 1px #b2ffff;
   border-radius: 50%;
+`;
+
+export const Phrase = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: ${dimension}px;
+
+  &:hover ${LightPoint} {
+    background-color: #00f7ff;
+    box-shadow: 0 0 32px 1px #00f7ff, 0 0 64px 1px #00f7ff;
+  }
 `;
